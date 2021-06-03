759f1f94b934edcd655d106c9f9d3171433b4569
loganjacob76 <nagolisback@gmail.com>
2021-06-03 17:46:44 -0600

    Setup Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user
+  before_action :authorized
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorized
+    redirect_to root_path, notice: 'You need to be logged in to access that page' unless current_user
+  end
 end
