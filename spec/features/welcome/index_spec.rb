96765520f5c309e8a481c28cf93f487ea869662a
joemecha <jwmecha@gmail.com>
2021-06-04 17:11:41 -0500

    Add test for welcome page and page content, sign in button

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Welcome Page" do 
+  describe "Welcome Page" do 
+    before :each do 
+      visit root_path 
+    end
+    it "Has a title and a brief description of the application" do 
+      expect(page).to have_content("Welcome to DownDraft")
+      expect(page).to have_content("Meet the perfect beer for your next adventure. DownDraft recommends beer based on your destination's forecast and elevation.")
+      expect(page).to have_button("Login with Google")
+    end
+  end
+end
