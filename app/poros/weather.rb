9856337e33cbd67689d6633d17542c0f548607a7
joemecha <jwmecha@gmail.com>
2021-06-06 23:40:14 -0500

    Format weather date to 'Sun 6'

--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -10,10 +10,19 @@ class Weather
   def initialize(data)
     @location_name = data[:location_name]
     @location_state = data[:location_state]
-    @date = data[:date]
+    @date = nice_date(data[:date])
     @max_temp = data[:max_temp]
     @min_temp = data[:min_temp]
     @description = data[:description]
     @icon = data[:icon]
   end
+
+  def nice_date(string)
+    stringified = DateTime.strptime(string, '%Y-%m-%e')
+    stringified.strftime("%a%e")
+  end
+
+  # def hyphen_date(string)
+  #   string.gsub(" ", "-")
+  # end
 end
