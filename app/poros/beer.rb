5d45eb98f430613f4ec32f20116a83a90176903a
joemecha <jwmecha@gmail.com>
2021-06-07 17:02:00 -0500

    Create Beer PORO

--- app/poros/beer.rb
+++ app/poros/beer.rb
@@ -1,18 +1,13 @@
 class Beer
-  # attr_reader :location_name,
-  #             :location_state,
-  #             :date,
-  #             :max_temp,
-  #             :min_temp,
-  #             :description,
-  #             :icon
-  #
-  # def initialize(data)
-  #   @location_name = data[:location_name]
-  #   @location_state = data[:location_state]
-  #   @max_temp = data[:max_temp]
-  #   @min_temp = data[:min_temp]
-  #   @description = data[:description]
-  #   @icon = data[:icon]
-  # end
+  attr_reader :name,
+              :description,
+              :abv,
+              :style
+  
+  def initialize(data)
+    @name = data[:name]
+    @description = data[:description]
+    @abv = data[:abv]
+    @style = data[:style]
+  end
 end
