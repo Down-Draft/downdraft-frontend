2133ffa0cdf11650e4dfa111f96f346959488423
joemecha <jwmecha@gmail.com>
2021-06-09 17:04:26 -0500

    Beer Service Spec

--- spec/services/beer_service_spec.rb
+++ spec/services/beer_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Beer Service' do
   describe '#instance method' do
     describe 'get_beers_data' do
-      it 'returns data for beers given location' do
+      xit 'returns data for beers given location' do
         location = '80303'
         results = BeerService.get_beers_data(location)
         expect(results[:data].class).to eq(Array)
@@ -14,20 +14,39 @@ RSpec.describe 'Beer Service' do
     end
   
     describe 'get_beer_data' do
-      it 'returns data for beer given location' do
+      xit 'returns data for beer given location' do
         location = '80303'
         results = BeerService.get_beer_data(location)
         expect(results[:data].class).to eq(Array)
         
         # add additional test lines here
         
-      end  
+      end
+    end 
+
+    describe 'search_beer_data' do
+      it 'returns data for beer based on name search', :vcr do
+        data = 'Cow'
+        results = BeerService.search_beer_data(data)
+        expect(results[:data].class).to eq(Array)
+        expect(results[:data].count).to eq(6)
+        expect(results[:data].last[:attributes][:name]).to eq('Spotted Cow')      
+      end 
+    end
+   
+    describe 'SAD Path - search_beer_data' do
+      it 'returns no data for beer based on name search', :vcr do
+        data = 'Crawfish'
+        results = BeerService.search_beer_data(data)
+        expect(results.class).to eq(Hash)
+        expect(results[:errors]).to eq('No beers found')
+      end 
     end
   end 
 
   describe ".class methods" do
     describe 'conn' do 
-      it 'creates a new Faraday for the backend url/endpoint' do 
+     xit 'creates a new Faraday for the backend url/endpoint' do 
         
       end
     end
