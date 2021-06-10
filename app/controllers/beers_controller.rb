50b8d263071824fd20721e4c24c21f82c9063dc9
joemecha <jwmecha@gmail.com>
2021-06-10 15:11:39 -0500

    Remove 'pry' from Beers Controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -3,7 +3,7 @@ class BeersController < ApplicationController
     @trip_id = params[:trip_id]
     if params[:search].present?
       @beers = BeerFacade.search_beer(params[:search])
-      require 'pry'; binding.pry
+      
     elsif @trip_id.present?
       @beers = BeerFacade.get_beers_data(@trip_id)
     end 
