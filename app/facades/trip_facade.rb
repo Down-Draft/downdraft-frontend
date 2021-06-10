e78aab2960fd2d2024b80985c41b853da6eb42a7
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-09 20:24:08 -0600

    Add get_single_trip and see_trip_index

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -1,5 +1,17 @@
 class TripFacade
   class << self
+    def get_single_trip(trip_id)
+      trip = TripsService.get_trip_show(trip_id)
+      Trip.new(trip)
+    end
+
+    def see_trip_index(user_id)
+      trips = TripsService.get_trips(user_id)
+      trips.map do |trip|
+        Trip.new(trip)
+      end
+    end
+
     def new_trip(info)
       trip = TripsService.create_trip(info)
       if trip.include?(:errors)
