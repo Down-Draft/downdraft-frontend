06ce852a66962166b6cd40b0699cb52e6c7cbd1f
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-09 20:25:34 -0600

    Refactor

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -3,9 +3,6 @@ class BeersController < ApplicationController
     if params[:search].present?
 
     else
-      require "pry"; binding.pry
-      @trip = TripFacade.get_trip
-      @beers = trip_id
     end
 
   end
